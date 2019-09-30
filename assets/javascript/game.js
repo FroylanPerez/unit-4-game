f2dc3bf11dc7fa06a7999307540f5d7fdaf6f8c4
Froylan Perez <fperez@noryan.com.mx>
2019-09-30 10:27:04 -0500

    Avance en mostrar numero random y asignar valor diferente a cada imagen, se muestra el total y da aviso de Win o Losse. Falta función de reinicio y conteo de ganados y perdidos

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,36 @@
 $(document).ready(function() {
 
-$(function() {
-    var random = Math.floor(Math.random() * 120)+1;
-    if(random >=19) {
+    var score = 0;
+    var numberOptions = [10, 5, 3, 1, 4, 7, 2, 9, 6];
+
+    var random = Math.floor(Math.random() * 102)+19;
         $("#randomNumber").text(random);
+
+    for (var i = 0; i < 4; i++) {
+        var imageCrystal = $("<img>");
+        var imageCrystal = numberOptions[Math.floor(Math.random() * numberOptions.length)];
+        console.log(i,imageCrystal)
+        $("#crystal-" + i).attr("data-crystalvalue", imageCrystal);
     }
-    else {
-        // $("#randomNumber").text("please refresh to get a valid number");
-        $("#randomNumber").trigger("reset");
-    }
-})
+
+    $(".crystal").on("click", function() {
+        console.log(this)
+        var crystalValue = ($(this).attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+        score += crystalValue;
+        console.log("New score: " + score);
+        $("#totalScore").html(score);
+
+        if (score === random) {
+            $("#alert").html("YOU WIN!!!");
+        }
+        else if (score >= random) {
+            $("#alert").html("YOU LOOSE!!!");
+        }
+    });
+       
+
+
 
 
 
