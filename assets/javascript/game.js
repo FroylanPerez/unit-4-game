311184f202e62a9082fb2a8dbbc02fb6ee4c7f95
Froylan Perez <fperez@noryan.com.mx>
2019-10-01 15:10:08 -0500

    Versión final con estilo y detalles resueltos

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 
     var score = 0;
-    var numberOptions = [10, 5, 3, 1, 4, 7, 2, 9, 6];
+    var win = 0;
+    var lose = 0;
+    var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
     var random = Math.floor(Math.random() * 102)+19;
         $("#randomNumber").text(random);
@@ -20,13 +22,35 @@ $(document).ready(function() {
         score += crystalValue;
         console.log("New score: " + score);
         $("#totalScore").html(score);
+        $("#alert").html("");
+
 
         if (score === random) {
             $("#alert").html("YOU WIN!!!");
+            win ++;
+            $("#win").html("Wins: " + win);
+            reset();
         }
         else if (score >= random) {
             $("#alert").html("YOU LOOSE!!!");
+            lose ++;
+            $("#lose").html("Losses: " + lose);
+            reset();
         }
+
+    function reset () {
+        score = 0;
+        $("#totalScore").html(score);
+        random = Math.floor(Math.random() * 102)+19;
+        $("#randomNumber").text(random);
+        for (var i = 0; i < 4; i++) {
+            var imageCrystal = $("<img>");
+            var imageCrystal = numberOptions[Math.floor(Math.random() * numberOptions.length)];
+            console.log(i,imageCrystal)
+            $("#crystal-" + i).attr("data-crystalvalue", imageCrystal);
+        }
+    }
+
     });
        
 
